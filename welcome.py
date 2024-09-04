af85c41b2748361c8bacf0baa99ffd6313a8f040
Lakshay Kalra <spambotofficial44@gmail.com>
2024-09-04 19:05:53 -0400

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -17,12 +17,13 @@ st.markdown('<style>div.block-container{padding-top:3rem;font-family: "montserra
 
 
 st.header("Information")
-st.caption("This website is for NBA enthusiasts. Wanna find out information about your favourite player? Wanna know how his upcoming season will go? or just a data enthusiast?")
-st.caption("You've come to the right place!")
-st.caption("Click on the top left to access 'playerdata' or 'visonAI' our insane AI that predicts how your favourite player's upcoming season is gonna go, I can see the gambling enthusiast in you full of joy already :smile")
+st.write("This website is for NBA enthusiasts. Wanna find out information about your favourite player? Wanna know how his upcoming season will go? or just a data enthusiast?")
+st.write("You've come to the right place!")
+st.write("Click on the top left to access 'playerdata' or 'visonAI' our insane AI that predicts how your favourite player's upcoming season is gonna go, I can see the gambling enthusiast in you full of joy already :smile")
 
 st.divider()
+url = "www.linkedin.com/in/lakshay-kalra-b13121198"
 
 st.header("About ME")
-st.caption("HI, I'm Lakshay Kalra - the creator of this website!")
-st.caption("You can find out more about me here")
+st.write("HI, I'm Lakshay Kalra - the creator of this website!")
+st.write("Find out more about me [here](%s)!" % url)
